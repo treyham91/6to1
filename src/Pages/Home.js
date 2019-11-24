a2f9f090a91e9632c109db098352aa33288c3709
Trey <trey.hamilton21@gmail.com>
2019-11-24 13:53:33 -0600

    home page final updates

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -8,13 +8,17 @@ import './home.css';
 
 
 const Home = (props) => {
-    const [behindTheName, setTheBehindTheName] = useState(
-        "Whether you're looking to add a new table to your dining room, or replace the old chairs in your kitchen, everything will be made the way you want it." +
-        "Whether you're looking to add a new table to your dining room, or replace the old chairs in your kitchen, everything will be made the way you want it." +
-        "Whether you're looking to add a new table to your dining room, or replace the old chairs in your kitchen, everything will be made the way you want it." +
-        "Whether you're looking to add a new table to your dining room, or replace the old chairs in your kitchen, everything will be made the way you want it." +
-        "Whether you're looking to add a new table to your dining room, or replace the old chairs in your kitchen, everything will be made the way you want it."
-    )
+    const [behindTheName, setTheBehindTheName] = useState([
+        "This company was created as a way for me to incorporate my two passions; woodworking and my family. I did work as a CNC machinist for 10 years and then" +
+        "quickly realized that I was not spending enough time with my family and I was unhappy continuing a job I did not love; this sparked the idea for my company.",
+
+        "The name 6to1 Designs, LLC came from the fact that I am married and have 5 daughters; there are six women to one man in our household. This name was perfect" +
+        "and the company took off.",
+
+        "At 6to1 Designs, LLC, we make anything from pallet signs for walls to large farmhouse kitchen tables, and everything in between. We provide quality customer " +
+        "service and ensure that our customers are involved in every step of the process when creating furniture for your home. Contact us to learn more about a new piece for " +
+        " your home!"
+    ])
 
     const adjustContent = () => {
         if (window.innerWidth < 1045 && window.innerWidth > 910) {
@@ -76,10 +80,12 @@ const Home = (props) => {
                 </div>
             </div>
             <div id="third-container" className="container-fluid">
-                <figure>
+                <figure style={{ maxWidth: 750, margin: 'auto' }}>
                     <img src={fam} id="third-container-image" alt="Wood Design" />
                     <figcaption id="third-container-header">Behind the Name</figcaption>
-                    <figcaption id="third-container-content">{behindTheName}</figcaption>
+                    <figcaption className="third-container-content">{behindTheName[0]}</figcaption><br />
+                    <figcaption className="third-container-content">{behindTheName[1]}</figcaption><br />
+                    <figcaption id="bottom-text" style={{ fontWeight: 'bold', backgroundColor: 'hsl(0, 0%, 90%)', borderRadius: 4, padding: 10 }} className="third-container-content">{behindTheName[2]}</figcaption>
                 </figure>
             </div>
             <div id="contact-container" className="container-fluid">
