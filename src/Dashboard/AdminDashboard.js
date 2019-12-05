64a4834e3d3214748fd9eb345093202221268cf2
Trey <trey.hamilton21@gmail.com>
2019-12-05 01:42:43 -0600

    dashboard updates

--- src/Dashboard/AdminDashboard.js
+++ src/Dashboard/AdminDashboard.js
@@ -30,19 +30,19 @@ const AdminDashboard = (props) => {
                             </div>
                         </div>
                         <div style={{ display: 'grid', gridGap: 15, padding: 10, gridTemplateColumns: 'auto auto auto auto', gridTemplateRows: 'auto auto auto auto' }}>
-                            <div style={{ border: '1px solid gray', height: 75, borderRadius: 4, backgroundColor: 'white', textTransform: 'none', padding: 10, textAlign: 'left' }}>Current Projects</div>
-                            <div style={{ border: '1px solid gray', height: 75, borderRadius: 4, backgroundColor: 'white', textTransform: 'none', padding: 10, textAlign: 'left' }}>Accounts Receivable</div>
-                            <div style={{ border: '1px solid gray', height: 75, borderRadius: 4, backgroundColor: 'white' }}></div>
-                            <div style={{ border: '1px solid gray', height: 75, borderRadius: 4, backgroundColor: 'white' }}></div>
-                            <div style={{ border: '1px solid gray', height: 375, borderRadius: 4, backgroundColor: 'white', gridColumn: '1/4' }}></div>
-                            <div style={{ border: '1px solid gray', height: 375, borderRadius: 4, backgroundColor: 'white', gridColumn: '4/5' }}></div>
-                            <div style={{ border: '1px solid gray', height: 375, borderRadius: 4, backgroundColor: 'white' }}></div>
-                            <div style={{ border: '1px solid gray', height: 375, borderRadius: 4, backgroundColor: 'white', gridColumn: '2/5' }}></div>
+                            <div className="dashboard-modal" style={{ height: 75 }}>Current Projects</div>
+                            <div className="dashboard-modal" style={{ height: 75 }}>Accounts Receivable</div>
+                            <div className="dashboard-modal" style={{ height: 75 }}></div>
+                            <div className="dashboard-modal" style={{ height: 75 }}></div>
+                            <div className="dashboard-modal" style={{ height: 375, gridColumn: '1/4' }}></div>
+                            <div className="dashboard-modal" style={{ height: 375, gridColumn: '4/5' }}></div>
+                            <div className="dashboard-modal" style={{ height: 375 }}></div>
+                            <div className="dashboard-modal" style={{ height: 375, gridColumn: '2/5' }}></div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div >
+        </div>
     )
 }
 
