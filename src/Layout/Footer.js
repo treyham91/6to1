1c06d5f8107abcb5724b77e879f620e3130c41da
Trey <trey.hamilton21@gmail.com>
2019-12-09 23:35:43 -0600

    footer updates

--- src/Layout/Footer.js
+++ src/Layout/Footer.js
@@ -9,8 +9,8 @@ const Footer = (props) => {
         <div className="footer">
             <div style={{ display: 'flex' }}>
                 <div style={{ margin: 'auto' }}>
-                    <Icon target="_blank" onClick={() => window.location.href = "https://www.instagram.com/6to1designs/"} className="social-icon" type="instagram" />
-                    <Icon target="_blank" onClick={() => window.location.href = "https://www.facebook.com/pg/6to1designs"} className="social-icon" type="facebook" />
+                    <Icon onClick={() => window.location.href = "https://www.instagram.com/6to1designs/"} className="social-icon" type="instagram" />
+                    <Icon onClick={() => window.location.href = "https://www.facebook.com/pg/6to1designs"} className="social-icon" type="facebook" />
                     <p style={{ color: 'white', fontSize: 8, marginTop: 100 }}>
                         Published on November 24, 2019
                             <img style={{ height: 20, width: 20, marginLeft: 10 }} src={logo} atl="Cannot be displayed" />
