3dbd8b695175d0ef6c36fdae8abc1a8b7daecd15
Trey Hamilton <trey.hamilton@uponor.com>
2019-12-28 11:10:21 -0600

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/6to1/precache-manifest.8e0f0502c7cf502e746794b12cc6f9d0.js"
+  "/6to1/precache-manifest.0d9208b1ff89cdb091706df6762af644.js"
 );
 
 self.addEventListener('message', (event) => {
